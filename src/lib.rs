lu-zero/autotools-rs

commit 256dc858be08abb5d6423eaa0be458e6a26e680f

Accept "-cc" prefix when determining target triple
Compilers are often called e.g. "x86_64-unknown-linux-gnu-cc" rather
than "x86_64-unknown-linux-gnu-gcc".

src/lib.rs
@@ -555,15 +555,18 @@ impl Config {
             args.push(os.to_string_lossy().to_string());
         }
 
-        if !config_host {
-            let compiler_path = format!("--host={}", c_compiler.path().display());
-            if compiler_path != "--host=musl-gcc" && compiler_path.ends_with("-gcc") {
-                args.push(compiler_path[0..compiler_path.len() - 4].to_string());
+        let cc_path = c_compiler.path().to_str().unwrap();
+        let cxx_path = c_compiler.path().to_str().unwrap();
+
+        if !config_host && cc_path != "musl-gcc" {
+            let host = cc_path.strip_suffix("-cc").or_else(|| cc_path.strip_suffix("-gcc"));
+            if let Some(host) = host {
+                args.push(format!("--host={}", host));
             }
         }
 
-        cmd.env("CC", c_compiler.path().to_str().unwrap());
-        cmd.env("CXX", cxx_compiler.path().to_str().unwrap());
+        cmd.env("CC", cc_path);
+        cmd.env("CXX", cxx_path);
 
         for &(ref k, ref v) in c_compiler.env().iter().chain(&self.env) {
             cmd.env(k, v);

commit 0ae55e84a3365bae581b4e980899c1ed89b8e22b

Fix typo in determining cxx compiler path
This typo was introduced in #27.

src/lib.rs
@@ -556,7 +556,7 @@ impl Config {
         }
 
         let cc_path = c_compiler.path().to_str().unwrap();
-        let cxx_path = c_compiler.path().to_str().unwrap();
+        let cxx_path = cxx_compiler.path().to_str().unwrap();
 
         if !config_host && cc_path != "musl-gcc" {
             let host = cc_path.strip_suffix("-cc").or_else(|| cc_path.strip_suffix("-gcc"));